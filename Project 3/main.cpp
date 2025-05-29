ef6c70f0d5e4ee7ccc1f02575b3d27a275fa4fd5
Szymateu <lewickiszymon1827@gmail.com>
2025-05-29 21:28:05 +0200

    update dft and idft

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -79,27 +79,28 @@ public:
     std::vector<double> x;
     std::vector<double> y;
     std::vector<std::complex<double>> j;
+    int samples;
 
     ResultVector() = default;
-    ResultVector(const std::vector<double>& x_plot, const std::vector<double>& y_plot, const std::vector<std::complex<double>>& complex) :x(x_plot), y(y_plot), j(complex) {};
+    ResultVector(const std::vector<double>& x_plot, const std::vector<double>& y_plot, const std::vector<std::complex<double>>& complex, double samples) :x(x_plot), y(y_plot), j(complex), samples(samples) {};
 
     ResultVector operator+(const ResultVector& other) const { //jeœli ta sama wielkoœæ wektorów to zsumuj je za pomoc¹ (przeci¹¿onego) "+"
         std::vector<double> sum(y.size());
         if (x != other.x) {
             std::cout << "sizes differ! ";
             sum = { 0 };
-            return ResultVector(x, sum, cplx0);
+            return ResultVector(x, sum, cplx0, samples);
         }
         std::transform(y.begin(), y.end(), other.y.begin(), sum.begin(),
             [](double v1, double v2) {return (v1 + v2); });
-        return ResultVector(x, sum, cplx0);
+        return ResultVector(x, sum, cplx0, samples);
     }
 
     ResultVector operator*(double scalar) const {//this -> Result vector, czyli x i y, other -> scalar
         std::vector<double>multiplication(y.size());
         std::transform(y.begin(), y.end(), multiplication.begin(),
             [scalar](double v0) {return (scalar * v0); });
-        return ResultVector(x, multiplication, cplx0); // wynik wektor * skalar
+        return ResultVector(x, multiplication, cplx0, samples); // wynik wektor * skalar
     }
 
     ResultVector3D mul(ResultVector plot2, double startX, double endX, double startY, double endY, int samples) {
@@ -112,7 +113,6 @@ public:
                 Z[a][b] = y[b] * plot2.y[a];
             }
         }
-
         ResultVector3D result(X, Y, Z);
         return result;
     }
@@ -127,10 +127,11 @@ namespace py = pybind11;
 PYBIND11_MODULE(_core, m) {
     py::class_<ResultVector>(m, "ResultVector")
         .def(py::init<>())
-        .def(py::init<const std::vector<double>&, const std::vector<double>&, const std::vector<std::complex<double>>&>())
+        .def(py::init<const std::vector<double>&, const std::vector<double>&, const std::vector<std::complex<double>>&, double>())
         .def_readwrite("x", &ResultVector::x)
         .def_readwrite("y", &ResultVector::y)
         .def_readwrite("j", &ResultVector::j)
+        .def_readwrite("samples", &ResultVector::samples)
         .def("__add__", &ResultVector::operator+) //vector + vector, jeœli te same end start i samples
         .def("__mul__", &ResultVector::operator*) //vector * scalar
         .def("__rmul__", [](const ResultVector& vec, double scalar) { //rmul bierze na odwrót argumenty, czyli taki zapis odpowiada scalar * vector
@@ -231,7 +232,7 @@ PYBIND11_MODULE(_core, m) {
     )pbdoc");
 
     m.def("add", &add,
-        py::arg("i"), py::arg("j"),  R"pbdoc(
+        py::arg("i"), py::arg("j"), R"pbdoc(
         Add two numbers
     )pbdoc");
 
@@ -247,7 +248,7 @@ PYBIND11_MODULE(_core, m) {
         for (double val : fsin) {
             ysin.push_back(std::sin(val * f));
         }
-        ResultVector sinPlot(fsin, ysin, cplx0);
+        ResultVector sinPlot(fsin, ysin, cplx0, samples);
         return sinPlot;
         }, py::arg("frequency"), py::arg("start"), py::arg("end"), py::arg("samples"), R"pbdoc(
         Create sinus plot
@@ -260,7 +261,7 @@ PYBIND11_MODULE(_core, m) {
         for (double val : fcos) {
             ycos.push_back(std::cos(val * f));
         }
-        ResultVector cosPlot(fcos, ycos, cplx0);
+        ResultVector cosPlot(fcos, ycos, cplx0, samples);
         return cosPlot;
         }, py::arg("frequency"), py::arg("start"), py::arg("end"), py::arg("samples"), R"pbdoc(
         Create cosinus plot
@@ -275,7 +276,7 @@ PYBIND11_MODULE(_core, m) {
             else if (std::sin(val * f) > 0) ysqw.push_back(1);
             else ysqw.push_back(0);
         }
-        ResultVector sqwPlot(fsqw, ysqw, cplx0);
+        ResultVector sqwPlot(fsqw, ysqw, cplx0,sample);
         return sqwPlot;
         }, py::arg("frequency"), py::arg("start"), py::arg("end"), py::arg("samples"), R"pbdoc(
         Create square wave plot
@@ -287,28 +288,35 @@ PYBIND11_MODULE(_core, m) {
         for (double val : fsaw) {
             ysaw.push_back((std::fmod((f * (val / matplot::pi)), (2.0))) - 1);
         }
-        ResultVector sawPlot(fsaw, ysaw, cplx0);
+        ResultVector sawPlot(fsaw, ysaw, cplx0,sample);
         return sawPlot;
         }, py::arg("frequency"), py::arg("start"), py::arg("end"), py::arg("samples"), R"pbdoc(
         Create sawwave plot
     )pbdoc");
 
-    m.def("fourier", [](ResultVector testPlot, double start, double end, int seqNr) {
-        std::vector<double> x = matplot::linspace(start, end, seqNr);
+    m.def("fourier", [](ResultVector testPlot) {
+        double size = testPlot.x[(testPlot.x.size() - 1) - testPlot.x[0]];
+        int samples = testPlot.x.size();
+        std::vector<double> x(samples);
         std::vector<double> y;
+        int halfSampl = samples / 2;
+        for (int i = 0; i < samples; ++i) {
+            x[i] = i;
+        }
         std::vector<std::complex<double>> complexVec;
-        for (int k = 0; k < seqNr; ++k) {
+        for (int k = 0; k < samples; ++k) {
             std::complex<double> fourierX = 0;
-            for (int n = 0; n < seqNr; ++n) {
-                double angle = 2.0 * matplot::pi * (double(k) / double(seqNr)) * n;
+            for (int n = 0; n < samples; ++n) {
+                double angle = 2.0 * matplot::pi * (double(k) / double(samples)) * n;
                 fourierX += testPlot.y[n] * (std::exp(-i * angle));
             }
             complexVec.push_back(fourierX);
-            y.push_back(std::abs(fourierX));
+            if (k < halfSampl)y.push_back(std::abs(fourierX));
+            else y.push_back(0);
         }
-        ResultVector dftPlot(x, y, complexVec);
+        ResultVector dftPlot(x, y, complexVec, size);//tutaj size to rozmiar pierwotnej funkcji - tak lepiej żeby było oryginalnie a samples pobierać z rozmiaru wektora x
         return dftPlot;
-        }, py::arg("plot"), py::arg("start"), py::arg("end"), py::arg("samples"), R"pbdoc(
+        }, py::arg("plot"), R"pbdoc(
         Discrete Fourier transform
     )pbdoc");
 
@@ -318,13 +326,13 @@ PYBIND11_MODULE(_core, m) {
             range2pi = 1;
             end = sampleSize - 1;
         }
-        std::vector<double> x = matplot::linspace(0, end, range2pi*end+1);
+        std::vector<double> x = matplot::linspace(0, end, range2pi * end + 1);
         std::vector<double> y;
 
         for (int i = 0; i < range2pi * (end + 1); i++)
             y.push_back(testPlot.y[i]);
 
-        ResultVector shortPlot(x, y, testPlot.j);
+        ResultVector shortPlot(x, y, testPlot.j,sampleSize);
         return shortPlot;
 
         }, py::arg("plot"), py::arg("end"), py::arg("sampleSize"), py::arg("rangeBy2pi"), R"pbdoc(
@@ -335,9 +343,9 @@ PYBIND11_MODULE(_core, m) {
         frequencies only show if they are equal to (n/rangeBy2pi), where n is any natural number
     )pbdoc");
 
-    m.def("inv_fourier", [](ResultVector testPlot, double start, double end) {
+    m.def("inv_fourier", [](ResultVector testPlot) {
         int seqNr = testPlot.x.size();
-        std::vector<double> x = matplot::linspace(start, end, seqNr);
+        std::vector<double> x = matplot::linspace(0, testPlot.samples, seqNr);
         std::vector<double> y;
         std::vector<std::complex<double>> complexVec;
         for (int k = 0; k < seqNr; ++k) {
@@ -349,9 +357,9 @@ PYBIND11_MODULE(_core, m) {
             y.push_back((1.0 / seqNr) * (inv_fourierX.real()));
             complexVec.push_back((1.0 / seqNr) * inv_fourierX);
         }
-        ResultVector idftPlot(x, y, complexVec);
+        ResultVector idftPlot(x, y, complexVec, testPlot.samples);
         return idftPlot;
-        }, py::arg("DFT"), py::arg("start"), py::arg("end"), R"pbdoc(
+        }, py::arg("DFT"), R"pbdoc(
         Inverted discrete Fourier transform
     )pbdoc");
 
@@ -386,30 +394,31 @@ PYBIND11_MODULE(_core, m) {
     )pbdoc");
 
     m.def("filter1D", [](ResultVector testPlot) {
-            std::vector<double> filter = {0.006, 0.061, 0.242, 0.383, 0.242, 0.061, 0.006};// filtr gaussa
-            std::vector<double> out_plot;
-            int filter_size = filter.size();
-            int offset = filter_size / 2;
-            int n = testPlot.y.size();
-            double sum;
-            for (int k = 0; k < n; ++k) {
-                sum = 0;
-                for (int r = 0; r < filter_size; ++r) {
-                    int plot_index = k + r - offset;
-                    if (plot_index >= 0 && plot_index < n) {
-                        sum += (testPlot.y[plot_index] * filter[r]);
-                    } else {
-                        int padding;
-                        if (plot_index < 0) padding = 0;
-                        else if (plot_index > (n - 1)) padding = (n - 1);
-                        else padding = plot_index;
-                        sum += testPlot.y[padding] * filter[r];
-                    }
+        std::vector<double> filter = { 0.006, 0.061, 0.242, 0.383, 0.242, 0.061, 0.006 };// filtr gaussa
+        std::vector<double> out_plot;
+        int filter_size = filter.size();
+        int offset = filter_size / 2;
+        int n = testPlot.y.size();
+        double sum;
+        for (int k = 0; k < n; ++k) {
+            sum = 0;
+            for (int r = 0; r < filter_size; ++r) {
+                int plot_index = k + r - offset;
+                if (plot_index >= 0 && plot_index < n) {
+                    sum += (testPlot.y[plot_index] * filter[r]);
                 }
-                out_plot.push_back(sum);
+                else {
+                    int padding;
+                    if (plot_index < 0) padding = 0;
+                    else if (plot_index > (n - 1)) padding = (n - 1);
+                    else padding = plot_index;
+                    sum += testPlot.y[padding] * filter[r];
+                }
             }
-            ResultVector filteredPlot(testPlot.x, out_plot, testPlot.j);
-            return filteredPlot;
+            out_plot.push_back(sum);
+        }
+        ResultVector filteredPlot(testPlot.x, out_plot, testPlot.j, testPlot.samples);
+        return filteredPlot;
         }, py::arg("plot"), R"pbdoc(
             Apply filter to plot
     )pbdoc");
@@ -538,25 +547,25 @@ PYBIND11_MODULE(_core, m) {
 
     m.def("correlation", [](ResultVector testPlot1, ResultVector testPlot2) {
         std::vector<double> out_plot;
-        if(testPlot1.x != testPlot2.x) {
+        if (testPlot1.x != testPlot2.x) {
             std::cout << "sizes differ!";
             out_plot = { 0 };
-            return ResultVector(testPlot1.x, out_plot, cplx0);
-	}
-	int plotSize = testPlot1.y.size();
+            return ResultVector(testPlot1.x, out_plot, cplx0, testPlot1.samples);
+        }
+        int plotSize = testPlot1.y.size();
         long double sum;
         for (int n = 0; n < plotSize; ++n) {
             sum = 0;
             for (int k = 0; k < plotSize; ++k) {
                 if (k + n < plotSize)
-		    sum += (testPlot1.y[k] * testPlot2.y[k + n]);
+                    sum += (testPlot1.y[k] * testPlot2.y[k + n]);
             }
-	    out_plot.push_back(sum);
+            out_plot.push_back(sum);
         }
 
-	ResultVector correlationPlot(testPlot1.x, out_plot, cplx0);
+        ResultVector correlationPlot(testPlot1.x, out_plot, cplx0, testPlot1.samples);
         return correlationPlot;
-    	}, py::arg("plot1"), py::arg("plot2"), R"pbdoc(
+        }, py::arg("plot1"), py::arg("plot2"), R"pbdoc(
             Create correlation plot
     )pbdoc");
 
